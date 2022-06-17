c7c75994c9f8ebeb6596c7da2513791c3a2920e4
Oluwafemi_Tom <Oluwafemi_Tom@Oluwafemi-Tom.local>
2022-06-17 13:12:54 +0100

    first commit

---
+++ ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Oluwafemi Moses";
+            string reversedName = ReverseString(name);
+            Console.WriteLine(name + " written backward is: " + reversedName);
+        }
+
+
+        //Reverse for loop count end of the name to the beginning
+        private static string ReverseString(string name)
+        {
+            string r = "";
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                r += name[i];
+            }
+
+            return r;
+        }
+    }
+}
+
